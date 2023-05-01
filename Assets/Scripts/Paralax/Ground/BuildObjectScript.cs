40d7323d2d95d226ce9ff8ae0733215261522f44
josep93 <joseluisgomezmino@hotmail.com>
2023-05-01 14:34:09 +0200

    Minor changes

--- Assets/Scripts/Paralax/Ground/BuildObjectScript.cs
+++ Assets/Scripts/Paralax/Ground/BuildObjectScript.cs
@@ -18,17 +18,12 @@ public class BuildObjectScript : MonoBehaviour
     {
         cam = Camera.main.gameObject;
         startPos = transform.position.x;
-        //Invoke(nameof(Deactivate), 5);
     }
 
     private void FixedUpdate()
     {
         float dist = cam.transform.position.x * paralaxEffect;
         transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
-
-        //distanceCamera = transform.position.x - cam.transform.position.x;
-        //if (transform.position.x - cam.transform.position.x < -60) { Deactivate(); }
-        //if (transform.position.x - package.transform.position.x > 100) { Deactivate(); }
     }
 
     private void OnDisable()
@@ -45,11 +40,5 @@ public class BuildObjectScript : MonoBehaviour
     public void SetParalaxEffect(float paralaxEffect)
     {
         this.paralaxEffect = paralaxEffect;
-    }
-
-
-    private void Deactivate()
-    {
-        this.gameObject.SetActive(false);
     }
 }
